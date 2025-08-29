61cde0348bb3e347b1494ee60dc6b19c8da90c0b
Roman Matrosov <roman.matrosov.frontend@yandex.ru>
2025-08-29 19:09:45 +0300

    Add dynamic protocol in admin.js

--- app/static/js/admin.js
+++ app/static/js/admin.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://' + window.location.hostname + ':8000';
+const API_BASE = window.location.protocol + '//' + window.location.host;
 
 // Единый обработчик DOM
 document.addEventListener('DOMContentLoaded', async () => {
